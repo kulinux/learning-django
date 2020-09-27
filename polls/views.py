92c63eba0da761e70bfd0d604e956f6aab176423
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2020-09-27 07:41:14 +0200

    init

---
+++ polls/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello, world. Poll index")
+
+
+def detail(request, question_id):
+    return HttpResponse(
+        "You're looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse(
+        "You're looking at the results of %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(
+        "You're voting on question %s" % question_id)
